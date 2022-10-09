cd5abe3bde33131106ff10d5a54fadfdbe68ebbf
Juan Rodriguez Moreno <juanseromo12@gmail.com>
2022-10-08 22:06:10 -0500

    capacitor added, production bug fixed

---
+++ web_app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.monserrauth.app',
+  appName: 'monserrauth',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
